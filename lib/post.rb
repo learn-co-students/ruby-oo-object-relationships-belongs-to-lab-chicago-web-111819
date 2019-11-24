ceb1e70e4a83ee1f68c337969a41959b2d726b05
Andrea <andreabarr87@gmail.com>
2019-11-24 12:30:37 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+# require author.rb 
+
+class Post
+attr_accessor :title, :author
+
+@@all = []
+
+    def initialize
+        @title = title
+        @author = author
+        @@all << self
+    end
+
+    # def author
+       
+    # end
+
+
+
+
+
+end
